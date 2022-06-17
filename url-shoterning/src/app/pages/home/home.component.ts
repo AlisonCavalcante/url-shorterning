877755445cb8d2e7f7ed42f008c167c454396082
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-17 16:15:00 -0300

    obtendo e exibindo as urls encurtadas

--- url-shoterning/src/app/pages/home/home.component.ts
+++ url-shoterning/src/app/pages/home/home.component.ts
@@ -1,15 +1,27 @@
+import { UrlService } from './../../services/url.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  urls: any[] = [];
+  constructor(private urlService: UrlService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getUrlShortening(url: string) {
+    this.urlService.getUrl(url).subscribe((res) => {
+      if(this.urls.length >= 4){
+        this.urls.unshift(res);
+      } else {
+        this.urls.push(res);
+      }
+    });
   }
-
+  copyUrl(urlCopyed: any){
+    urlCopyed.select();
+  }
 }
